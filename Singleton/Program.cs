41d1db584147fd93b0192b3eba8d7713d1211f4b
Raphael Duarte <raphaelduartessph@gmail.com>
2023-03-28 15:44:36 -0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton jogador_1 = Singleton.GetInstance;
+            jogador_1.Message("A bola esta comigo no meio do campo");
+
+            Singleton jogador_2 = Singleton.GetInstance;
+            jogador_2.Message("Recebeu a bola");
+
+            Singleton jogador_3 = Singleton.GetInstance;
+            jogador_3.Message("Jogador 3: recebeu o lancamento na linha de fundo");
+
+            Console.ReadKey();
+        }
+    }
+
+}
